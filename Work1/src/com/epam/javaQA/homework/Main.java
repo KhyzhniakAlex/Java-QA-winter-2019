7c59b393a68ffe57d21f5befc5dfd0d88c71bb8f
KhyzhniakAlex <sasha.khyzhniak@gmail.com>
2019-02-14 21:39:35 +0200

    added first homework

---
+++ Work1/src/com/epam/javaQA/homework/Main.java
@@ -0,0 +1,19 @@
+package com.epam.javaQA.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        WriteEPAM();
+    }
+
+    private static void WriteEPAM()
+    {
+        //the amount of stars is not the same, because this variant is better
+        System.out.println("*****  *****      *      *     *");
+        System.out.println("*      *   *     * *     * * * *");
+        System.out.println("***    *****    *   *    *  *  *");
+        System.out.println("*      *       *******   *     *");
+        System.out.println("*****  *      *       *  *     *");
+    }
+}
