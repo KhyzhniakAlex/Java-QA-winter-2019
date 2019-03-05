0f5cc01c2748f2dfe5e955926ceb9737971c352e
KhyzhniakAlex <sasha.khyzhniak@gmail.com>
2019-03-05 22:54:07 +0200

    remake of work4 in both tasks

--- Work4/src/com/epam/javaQA/homework/Main.java
+++ Work4/src/com/epam/javaQA/homework/Main.java
@@ -18,12 +18,7 @@ public class Main {
                     System.out.println(turnIntoBinary());
                     break;
                 case "2":
-                    char[] gained = divideOnChar();
-                    for (char letter : gained) {
-
-                        System.out.print(letter + " ");
-                    }
-                    System.out.println();
+                    System.out.println(divideOnChar());
                     break;
                 case "3":
                     return;
@@ -41,9 +36,17 @@ public class Main {
         try {
             System.out.print("Write a integer: ");
             int number = in.nextInt();
-            if(number < 0) number = -number;
 
-            return Integer.toString(number, 2);
+            int temp;
+            String result = "";
+            while (number != 0) {
+
+                temp = number % 2;
+                result = temp + result;
+                number /= 2;
+            }
+
+            return result;
         }
         catch(Exception e) {
             System.err.println("Invalid typed value");
@@ -51,12 +54,29 @@ public class Main {
         return "";
     }
 
-    private static char[] divideOnChar() {
+    private static String divideOnChar() {
 
         Scanner in = new Scanner(System.in);
         System.out.print("Write a string: ");
-        String str = in.next();
+        String str = in.nextLine();
 
-        return str.toCharArray();
+        String result = "";
+        int i = 0;
+        while(i < str.length()) {
+
+            char ch = str.charAt(i);
+            int j = 0;
+            boolean check = true;
+            while(j < str.length()) {
+
+                if (ch == str.charAt(j) && i != j) check = false;
+                j++;
+            }
+            if (check) result += ch;
+            i++;
+        }
+
+        return result;
     }
 }
+
